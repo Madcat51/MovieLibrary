3fa1044ca7e714c9fe7dc0f467ac729c71111db4
Madcat51 <51@list.ru>
2021-12-13 17:23:24 +0300

    add snackbar and layout group1_item_movie-land

--- app/src/main/java/site/madcat/movielibrary/App.kt
+++ app/src/main/java/site/madcat/movielibrary/App.kt
@@ -1,8 +1,12 @@
 package site.madcat.movielibrary
 
 import android.app.Application
+import android.content.Context
 import site.madcat.movielibrary.domain.MovieRepository
 
 class App : Application() {
     val repository=MovieRepository()
 }
+
+val Context.app
+    get()=applicationContext as App
